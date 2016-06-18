378e588a32d62251dd21d0fedb0275b7c9c12018
chenl <cl1099217351@163.com>
2016-06-18 16:21:19 +0800

    Feature 3:build a example.add note in readmine

---
+++ opencv-2.4.13/samples/c/example_cmake/CMakeFiles/opencv_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_example.dir/minarea.c.o"
+  "opencv_example.pdb"
+  "opencv_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/opencv_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
